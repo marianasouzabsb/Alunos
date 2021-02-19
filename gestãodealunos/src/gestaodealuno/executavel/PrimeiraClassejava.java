404c98966dc7db2e11c098f4f3c20c7c7e9e4e57
Mariana Oliveira <marisouza.cco@gmail.com>
2021-02-19 12:41:06 -0300

    add iterações de listas.

--- gestãodealunos/src/gestaodealuno/executavel/PrimeiraClassejava.java
+++ gestãodealunos/src/gestaodealuno/executavel/PrimeiraClassejava.java
@@ -1,13 +1,67 @@
 package gestaodealuno.executavel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
 import gestaodealunos.classes.Aluno;
+import gestaodealunos.classes.Disciplina;
 
 public class PrimeiraClassejava {
 
 	public static void main(String[] args) {
-		
-		Aluno aluno1 = new Aluno(); // faz referencia ao construtor
-		
-	}
 
-}
+		List<Aluno> alunos = new ArrayList<Aluno>();
+
+		for (int qtd = 1; qtd <= 2; qtd++) {
+			// Recebe os dados via teclado
+			String nome = JOptionPane.showInputDialog("Qual o nome do Aluno " + qtd + "?");
+			String idade = JOptionPane.showInputDialog("Digite a Idade do Aluno");
+			String dataNascimento = JOptionPane.showInputDialog("Digite data de nascimento");
+
+			Aluno aluno1 = new Aluno();// faz referencia ao construtor
+
+			aluno1.setNome(nome);
+			aluno1.setIdade(Integer.valueOf(idade));
+			aluno1.setDataNascimento(dataNascimento);
+
+			// Para adicionar Disciplinas
+			for (int pos = 1; pos <= 4; pos++) {
+				String nomeDisciplina = JOptionPane.showInputDialog("Digite disciplina " + pos + " ? ");
+				String notaDisciplina = JOptionPane.showInputDialog("Digite nota " + pos + " ? ");
+
+				Disciplina disciplina = new Disciplina();
+
+				disciplina.setDisciplina(nomeDisciplina);
+				disciplina.setNota(Double.valueOf(notaDisciplina));
+
+				aluno1.getDisciplinas().add(disciplina);
+			}
+			// Para Remover disciplinas
+
+			int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina");
+			if (escolha == 0) { // opção sim é 0
+				int continuarRemover = 0;
+				int posicao = 1;
+				while (continuarRemover == 0) {
+					String disciplinaRemover = JOptionPane
+							.showInputDialog("Qual disciplina deseja remover? 1,2,3 ou 4	");
+					aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
+					posicao++;
+					continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a Remover disciplinas? ");
+				} // fecha while
+			}
+			alunos.add(aluno1); // ADD ALUNO NA LISTA
+
+			for (Aluno aluno : alunos) {
+				if (aluno.getNome().equalsIgnoreCase("mari")) {
+				System.out.println(aluno);
+				System.out.println("REsultado: " + aluno.getAlunoResultado());
+				System.out.println("Resultado: " + aluno.getAlunoResultado());
+				System.out.println("==================================================");
+				break;
+				}
+			}
+		} // fecha For da lista de alunos
+	}// Fecha Main
+}// Fecha PrimeiraClassejava
--- gestãodealunos/src/gestaodealunos/classes/Aluno.java
+++ gestãodealunos/src/gestaodealunos/classes/Aluno.java
@@ -1,46 +1,119 @@
 package gestaodealunos.classes;
 
-public class Aluno{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aluno {
+
 	String nome;
 	int idade;
 	String dataNascimento;
 
-	
-	public Aluno() { // Cria dos dados na memoria / Construtor
-		// TODO Auto-generated constructor stub
+	/*------------------------------------------------*/
+
+	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
 	}
 
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+
+	/*------------------------------------------------*/
 
 	public String getNome() {
 		return nome;
 	}
 
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 
 	public int getIdade() {
 		return idade;
 	}
 
-
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-
 
 	public String getDataNascimento() {
 		return dataNascimento;
 	}
 
-
 	public void setDataNascimento(String dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
-	  
+
+	// Metódo para calcular a media o aluno
+
+	public double getMediaNota() {
+		double somaNotas = 0.0;
+
+		for (Disciplina disciplina : disciplinas) {
+			somaNotas += disciplina.getNota();
+		}
+
+		return somaNotas / disciplinas.size();
+	}
+
+	// Metódo para ver se esta aprovado
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();
+		if (media >= 70) {
+			return true;
+
+		} else {
+			return false;
+		}
+	}
+	//Metódo Aprovado, reprovado ou Recuperação
 	
-	}//Fecha Aluno
+	public String getAlunoResultado(){
+		double media = this.getMediaNota();
+		if (media >= 50) {
+			if (media >= 70) {
+				return "Aluno Aprovado";
+			}else {
+				return "Aluno em recuperação";
+			}
+		} else {
+			return "Aluno Reprovado";
+		}	
+	}
 
+	
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", disciplinas="
+				+ disciplinas + "]";
+	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+}// Fecha Class Aluno
